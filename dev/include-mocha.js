d4a7f1e672b3ec2b3026cb70992c44e76d375753
Spomni <mikhail.snitko@gmail.com>
2018-03-28 16:30:38 +0300

    #5.2.0 Should return an "Error" object if pass an invalid option.

--- dev/include-mocha.js
+++ dev/include-mocha.js
@@ -21,22 +21,158 @@
 
 if (window.includeMocha === undefined){ // the first load
   (function(){
-  
+
     /** @class IncludeMocha
+     * @desc Create a function for the easy setup of the test environment.
+     * @returns {IncludeMochaInstance}
      */
     function IncludeMocha(){
-      
-      self = function(){};
-      
+
+      /** @var {IncludeMochaInstance} self
+       * @private
+       * @inner
+       * @memberof IncludeMocha
+       */
+      self = function( option ){
+        //-- Check and init input param
+          //-- check "option"
+            if ( !isString(option)
+              && !isStringArray(option)
+              && !isSimpleObject(option)
+              && !isUndefined(option)
+              ){
+              return new Error('The option "option" should be a string, a string array, an object or "undefined".')
+            };
+          //-- check init "option"
+            if ( isString(option) ){
+              option = { "specPath": [option] };
+            } else if ( isArray(option) ){
+              option = { 'specPath': option };
+            } else if ( isUndefined(option) ){
+              option = {};
+            }
+          //-- check "option.specRoot"
+            if ( !isString( option.specRoot )
+              && !isUndefined( option.specRoot )
+              ){
+              return new Error('The option "option.specRoot" should be a string or "undefined".')
+            };
+            
+          //-- check "option.specPath"
+            if ( !isString(option.specPath)
+              && !isStringArray(option.specPath)
+              && !isUndefined(option.specPath)
+              ){
+              return new Error('The option "option.specPath" should be a string, a string array or "undefined".')
+            };
+
+          //-- check "option.scriptPath"
+            if ( !isString(option.scriptPath)
+              && !isStringArray(option.scriptPath)
+              && !isUndefined(option.scriptPath)
+              ){
+              return new Error('The option "option.scriptPath" should be a string, a string array or "undefined".')
+            };
+            
+          //-- check "option.libRoot"
+            if ( !isString( option.libRoot )
+              && !isUndefined( option.libRoot )
+              ){
+              return new Error('The option "option.libRoot" should be a string or "undefined".')
+            };
+            
+          //-- check "option.mochaPath"
+            if ( !isString( option.mochaPath )
+              && !isUndefined( option.mochaPath )
+              ){
+              return new Error('The option "option.mochaPath" should be a string or "undefined".')
+            };
+            
+          //-- check "option.chaiPath"
+            if ( !isString( option.chaiPath )
+              && !isUndefined( option.chaiPath )
+              ){
+              return new Error('The option "option.chaiPath" should be a string or "undefined".')
+            };
+            
+          //-- check "option.selfPath"
+            if ( !isString( option.selfPath )
+              && !isUndefined( option.selfPath )
+              ){
+              return new Error('The option "option.selfPath" should be a string or "undefined".')
+            };
+            
+          //-- check "option.cssRoot"
+            if ( !isString( option.cssRoot )
+              && !isUndefined( option.cssRoot )
+              ){
+              return new Error('The option "option.cssRoot" should be a string or "undefined".')
+            };
+            
+          //-- check "option.cssPath"
+            if ( !isString(option.cssPath)
+              && !isStringArray(option.cssPath)
+              && !isUndefined(option.cssPath)
+              ){
+              return new Error('The option "option.cssPath" should be a string, a string array or "undefined".')
+            };
+        //
+      };
+
       self.__proto__ = IncludeMocha.prototype;
       return self;
     }
-    
     IncludeMocha.prototype = Object.create(Function.prototype);
-    
+
     /** @var {IncludeMochaInstance} includeMocha - Загрузить и настроить тестовое окружение. Запустить выполнение тестов. */
     window.includeMocha = new IncludeMocha();
-  
+
+    function isString( rabbit ){
+      if ( typeof( rabbit ) !== "string" ) return false;
+      return true;
+    };
+
+    function isArray( rabbit ){
+      if ( rabbit instanceof Array ) return true;
+      return false;
+    };
+
+    function isSimpleObject( rabbit ){
+      if ( typeof( rabbit ) === "object"
+        && rabbit !== null
+        && rabbit.__proto__ == Object.prototype
+        ){
+        return true;
+      };
+      return false;
+    };
+
+    function isUndefined( rabbit ){
+      if ( rabbit === undefined ) return true;
+      return false;
+    };
+
+    /** Return true if the "rabbit" is an array of strings.
+     *
+     * @param {mixed} rabbit
+     *
+     * @returns {boolean}
+     *
+     * @requires isArray()
+     * @requires isString()
+     */
+    function isStringArray( rabbit ) {
+      if ( !isArray( rabbit ) ) return false;
+
+      for (var i=0; i<rabbit.length; i++){
+        if ( !isString( rabbit[i] ) ){
+          return false;
+        }
+      }
+
+      return true;
+    }
+
   })();
 } else if ( undefined ){ // the second load
 
--- dev/test/spec/include-mocha-spec.js
+++ dev/test/spec/include-mocha-spec.js
@@ -1,9 +1,262 @@
 describe('include-mocha.js', function(){
 
-  var assert = testAssert;
+  //-- Support vars and functions
+    var assert = testAssert;
 
-  it( 'Should define the global var "includeMocha".', function(){
+    function cleanEnvironment( when ){
+
+    };
+
+    function cleanIt( message, callback ){
+      cleanEnvironment( 'before' );
+      it( message, callback );
+      cleanEnvironment( 'after' );
+    };
+
+    /** Call the callback function for each standart data type. Pass an instance of the type as the option "type".
+      * @param {Function} callback( type )
+      * @returns {undefined}
+      */
+    function forEachType( callback ){
+
+      var typeArr = [
+        1,
+        "string",
+        null,
+        undefined,
+        ["", 1, null],
+        ['string', 'string'],
+        {}
+      ];
+
+      var res = function(){
+        typeArr.forEach(function(type, i, typeArr){
+          callback( type );
+        });
+      };
+
+      return res;
+    };
+
+    function isString( rabbit ){
+      if ( typeof( rabbit ) !== "string" ) return false;
+      return true;
+    };
+
+    function isArray( rabbit ){
+      if ( rabbit instanceof Array ) return true;
+      return false;
+    };
+
+    function isSimpleObject( rabbit ){
+      if ( typeof( rabbit ) === "object"
+        && rabbit !== null
+        && rabbit.__proto__ == Object.prototype
+        ){
+        return true;
+      };
+      return false;
+    };
+
+    function isUndefined( rabbit ){
+      if ( rabbit === undefined ) return true;
+      return false;
+    };
+
+    /** Return true if the "rabbit" is an array of strings.
+     *
+     * @param {mixed} rabbit
+     *
+     * @returns {boolean}
+     *
+     * @requires isArray()
+     * @requires isString()
+     */
+    function isStringArray( rabbit ) {
+      if ( !isArray( rabbit ) ) return false;
+
+      for (var i=0; i<rabbit.length; i++){
+        if ( !isString( rabbit[i] ) ){
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+  //--
+
+  cleanIt( 'Should define the global var "includeMocha".', function(){
     assert.isDefined(includeMocha);
+  });
+
+  describe( 'includeMocha( option )', function(){
+
+    describe( 'Should return an "Error" object if pass an invalid option. Valid conditions:', function(){
+
+      describe( 'option', function(){
+        cleanIt( 'Should be a string, a string array, an object or "undefined".', forEachType(function( type ){
+
+          if ( !isString(type)
+            && !isStringArray(type)
+            && !isSimpleObject(type)
+            && !isUndefined(type)
+            ){
+            var result = includeMocha(type);
+            assert.instanceOf( result, Error );
+          };
+        }));
+      });
+
+      describe( 'option.specRoot', function(){
+        cleanIt( 'Should be a string or "undefined".', forEachType(function(type){
+          if ( !isString(type)
+            && !isUndefined(type)
+            ){
+
+            var result = includeMocha({
+              'specRoot': type
+            });
+            assert.instanceOf( result, Error );
+
+          };
+        }));
+      });
+
+      describe( 'option.specPath', function(){
+
+        cleanIt( 'Should be a string, a string array or "undefined".', forEachType(function(type){
+
+          if ( !isString(type)
+            && !isStringArray(type)
+            && !isUndefined(type)
+            ){
+
+            var result = includeMocha({
+              'specPath': type
+            });
+            assert.instanceOf( result, Error );
+
+          };
+
+        }));
+      });
+      
+      describe( 'option.scriptPath', function(){
+      
+        cleanIt( 'Should be a string, a string array or "undefined".', forEachType(function(type){
+      
+          if ( !isString(type)
+            && !isStringArray(type)
+            && !isUndefined(type)
+            ){
+      
+            var result = includeMocha({
+              'scriptPath': type
+            });
+            assert.instanceOf( result, Error );
+      
+          };
+        }));
+      });
+      
+      describe( 'option.libRoot', function(){
+        cleanIt( 'Should be a string or "undefined".', forEachType(function(type){
+          if ( !isString(type)
+            && !isUndefined(type)
+            ){
+        
+            var result = includeMocha({
+              'libRoot': type
+            });
+            assert.instanceOf( result, Error );
+        
+          };
+        }));
+      });
+      
+      describe( 'option.mochaPath', function(){
+        cleanIt( 'Should be a string or "undefined".', forEachType(function(type){
+          if ( !isString(type)
+            && !isUndefined(type)
+            ){
+        
+            var result = includeMocha({
+              'mochaPath': type
+            });
+            assert.instanceOf( result, Error );
+        
+          };
+        }));
+      });
+      
+      describe( 'option.chaiPath', function(){
+        cleanIt( 'Should be a string or "undefined".', forEachType(function(type){
+          if ( !isString(type)
+            && !isUndefined(type)
+            ){
+        
+            var result = includeMocha({
+              'chaiPath': type
+            });
+            assert.instanceOf( result, Error );
+        
+          };
+        }));
+      });
+      
+      describe( 'option.selfPath', function(){
+        cleanIt( 'Should be a string or "undefined".', forEachType(function(type){
+          if ( !isString(type)
+            && !isUndefined(type)
+            ){
+        
+            var result = includeMocha({
+              'selfPath': type
+            });
+            assert.instanceOf( result, Error );
+        
+          };
+        }));
+      });
+      
+      describe( 'option.cssRoot', function(){
+        cleanIt( 'Should be a string or "undefined".', forEachType(function(type){
+          if ( !isString(type)
+            && !isUndefined(type)
+            ){
+        
+            var result = includeMocha({
+              'cssRoot': type
+            });
+            assert.instanceOf( result, Error );
+        
+          };
+        }));
+      });
+      
+      describe( 'option.cssPath', function(){
+      
+        cleanIt( 'Should be a string, a string array or "undefined".', forEachType(function(type){
+      
+          if ( !isString(type)
+            && !isStringArray(type)
+            && !isUndefined(type)
+            ){
+      
+            var result = includeMocha({
+              'cssPath': type
+            });
+            assert.instanceOf( result, Error );
+      
+          };
+      
+        }));
+      });
+
+
+
+    });
   });
 
 });
