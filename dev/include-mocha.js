e6dca159c44d0de7666c22df0dd5af1f35764cb0
spomni <mikhail.snitko@gmail.com>
2018-03-28 12:25:19 +0300

    #5.1 "include-mocha.js" should define the global var "includeMocha".

---
+++ dev/include-mocha.js
@@ -0,0 +1,45 @@
+/** @typedef {Function} IncludeMochaInstance
+ * @description Загрузить и настроить тестовое окружение. Запустить выполнение тестов. Вывести ошибку в тело документа, если невозможно загрузить любой из ресурсов.
+ *
+ * @param {string|string[]|Object} [option] - Путь или массив путей к файлам спецификаций или список опций.
+ * @param {string} [option.specRoot="spec/"] - Путь к директории файлов спецификаций. Относительно данного пути подключаются файлы спецификаций тестов.
+ * @param {string|string[]} [option.specPath=[]] - Путь либо массив путей к файлам спецификаций.
+ * @param {string|string[]} [option.scriptPath=[]] - Путь либо массив путей к файлам тестируемых js-скриптов.
+ * @param {string} [option.libRoot="lib"] - Путь к директории файлов модулей. Относительно этого пути подключаются все файлы модулей, определенные свойствами "mochaPath", "chaiPath" и "selfPath".
+ * @param {string} [option.mochaPath="mocha.js"] - Путь к файлу модуля "mocha.js".
+ * @param {string} [option.chaiPath="chai.js"] - Путь к файлу модуля "chai.js".
+ * @param {string} [option.selfPath="include-mocha.js"] - Путь к файлу данного скрипта. Необходим для работы внутренней реализации.
+ * @param {string} [option.cssRoot="css/"] - Путь к директории css-стилей. Относительно данного пути подключаются файлы css-стилей.
+ * @param {string|string[]} [option.cssPath="mocha.css"] - Путь или массив путей к файлам css-стилей.
+ * @param {string|Object} [option.mochaSetup="bdd"] - Параметры для настройки модуля "mocha.js".
+ *
+ * @returns {undefined|Error}
+ *
+ * @property {null|Runner} runner - Результат вызова "mocha.run()".
+ */
+//
+
+if (window.includeMocha === undefined){ // the first load
+  (function(){
+  
+    /** @class IncludeMocha
+     */
+    function IncludeMocha(){
+      
+      self = function(){};
+      
+      self.__proto__ = IncludeMocha.prototype;
+      return self;
+    }
+    
+    IncludeMocha.prototype = Object.create(Function.prototype);
+    
+    /** @var {IncludeMochaInstance} includeMocha - Загрузить и настроить тестовое окружение. Запустить выполнение тестов. */
+    window.includeMocha = new IncludeMocha();
+  
+  })();
+} else if ( undefined ){ // the second load
+
+} else { //the third load
+
+}
--- dev/test/spec/include-mocha-spec.js
+++ dev/test/spec/include-mocha-spec.js
@@ -1,7 +1,9 @@
 describe('include-mocha.js', function(){
 
-  it('test it', function(){
-  
+  var assert = testAssert;
+
+  it( 'Should define the global var "includeMocha".', function(){
+    assert.isDefined(includeMocha);
   });
 
 });
