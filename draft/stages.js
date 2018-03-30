29beebeea79d1f0fd28b967ed9ce69e7741741c7
Spomni <mikhail.snitko@gmail.com>
2018-03-31 00:18:33 +0300

    proccess

--- draft/stages.js
+++ draft/stages.js
@@ -1,12 +1,23 @@
 (function(){
 
   /** @constructor IncludeMocha
-   *
+   * @returns {IncludeMochaInstance}
+   * @hideconstructor
    */
   function IncludeMocha() {
 
     var self = function(){
 
+      /** The error flag
+       * @member {boolean} IncludeMocha#_hasError
+       * @private
+       */
+      self._hasError = false;
+      
+      /** The alias for {@link IncludeMocha._is}
+       * @member {Object} IncludeMocha~is
+       * @private
+       */
       var is = self._is;
 
       //-- preinit input param
@@ -21,14 +32,16 @@
       //-- validate input param
         //-- "option"
           if ( !is.simpleObject(option) ){
-            return new Error('The option "option" should be a string, a string array, an object or "undefined".')
+            self._complete( new Error('The option "option" should be a string, a string array, an object or "undefined".') );
+            return;
           };
 
         //-- "option.specRoot"
           if ( !is.string( option.specRoot )
             && !is.undefined( option.specRoot )
             ){
-            return new Error('The option "option.specRoot" should be a string or "undefined".')
+            self._complete( new Error('The option "option.specRoot" should be a string or "undefined".') );
+            return;
           };
 
         //-- "option.specPath"
@@ -36,7 +49,8 @@
             && !is.stringArray(option.specPath)
             && !is.undefined(option.specPath)
             ){
-            return new Error('The option "option.specPath" should be a string, a string array or "undefined".')
+            self._complete( new Error('The option "option.specPath" should be a string, a string array or "undefined".') );
+            return;
           };
           if ( is.string(option.specPath) ){
             option.specPath =  [option.specPath];
@@ -46,7 +60,8 @@
             && !is.stringArray(option.scriptPath)
             && !is.undefined(option.scriptPath)
             ){
-            return new Error('The option "option.scriptPath" should be a string, a string array or "undefined".')
+            self._complete( new Error('The option "option.scriptPath" should be a string, a string array or "undefined".') );
+            return;
           };
           if ( is.string(option.scriptPath) ){
             option.scriptPath =  [option.scriptPath];
@@ -55,35 +70,40 @@
           if ( !is.string( option.libRoot )
             && !is.undefined( option.libRoot )
             ){
-            return new Error('The option "option.libRoot" should be a string or "undefined".')
+            self._complete( new Error('The option "option.libRoot" should be a string or "undefined".') );
+            return;
           };
 
         //-- "option.mochaPath"
           if ( !is.string( option.mochaPath )
             && !is.undefined( option.mochaPath )
             ){
-            return new Error('The option "option.mochaPath" should be a string or "undefined".')
+            self._complete( new Error('The option "option.mochaPath" should be a string or "undefined".') );
+            return;
           };
 
         //-- "option.chaiPath"
           if ( !is.string( option.chaiPath )
             && !is.undefined( option.chaiPath )
             ){
-            return new Error('The option "option.chaiPath" should be a string or "undefined".')
+            self._complete( new Error('The option "option.chaiPath" should be a string or "undefined".') );
+            return;
           };
 
         //-- "option.selfPath"
           if ( !is.string( option.selfPath )
             && !is.undefined( option.selfPath )
             ){
-            return new Error('The option "option.selfPath" should be a string or "undefined".')
+            self._complete( new Error('The option "option.selfPath" should be a string or "undefined".') );
+            return;
           };
 
         //-- "option.cssRoot"
           if ( !is.string( option.cssRoot )
             && !is.undefined( option.cssRoot )
             ){
-            return new Error('The option "option.cssRoot" should be a string or "undefined".')
+            self._complete( new Error('The option "option.cssRoot" should be a string or "undefined".') );
+            return;
           };
 
         //-- "option.cssPath"
@@ -91,12 +111,30 @@
             && !is.stringArray(option.cssPath)
             && !is.undefined(option.cssPath)
             ){
-            return new Error('The option "option.cssPath" should be a string, a string array or "undefined".')
+            self._complete( new Error('The option "option.cssPath" should be a string, a string array or "undefined".') );
+            return;
           };
           if ( is.string(option.cssPath) ){
             option.cssPath =  [option.cssPath];
           }
       //
+      /** Contains initiated options
+       * @member {IncludeMocha#_option} IncludeMocha#_option
+       * @private
+       */
+        /** @typedef {Object} IncludeMocha#_option
+         * @desc Список опций, используемых экземпляром класса "IncludeMocha".
+         * @property {string} specRoot="spec/" - Путь к директории файлов спецификаций. Относительно данного пути подключаются файлы спецификаций тестов.
+         * @property {string[]} specPath=[]] - Массив путей к файлам спецификаций.
+         * @property {string[]} scriptPath=[]] - Массив путей к файлам тестируемых js-скриптов.
+         * @property {string} libRoot="lib" - Путь к директории файлов модулей. Относительно этого пути подключаются все файлы модулей, определенные свойствами "mochaPath", "chaiPath" и "selfPath".
+         * @property {string} mochaPath="mocha.js" - Путь к файлу модуля "mocha.js".
+         * @property {string} chaiPath="chai.js" - Путь к файлу модуля "chai.js".
+         * @property {string} selfPath="include-mocha.js" - Путь к файлу данного скрипта. Необходим для работы внутренней реализации.
+         * @property {string} cssRoot="css/" - Путь к директории css-стилей. Относительно данного пути подключаются файлы css-стилей.
+         * @property {string[]} cssPath="mocha.css"] - Путь или массив путей к файлам css-стилей.
+         * @property {string|Object} mochaSetup="bdd" - Параметры для настройки модуля "mocha.js".
+        */
       self._option = {
         'specRoot':   option.specRoot   || 'spec/',
         'specPath':   option.specPath   || [],
@@ -109,10 +147,30 @@
         'cssPath':    option.cssPath    || [],
         'mochaSetup': option.mochaSetup || 'bdd'
       };
-
+      
+      /** The object for resources register.
+       * @member {object} IncludeMocha#_resources
+       *
+       * @property {array[]} firstStage - The first stage resource list.
+       * @property {string[]} firstStage[]] - The resource data item.
+       * @property {'link'|'script'} firstStage[][0]] - The resource type.
+       * @property {string} firstStage.[][1]] - The resource path.
+       *
+       * @property {array[]} secondStage - The second stage resource list.
+       * @property {string[]} secondStage[]] - The resource data item.
+       * @property {'link'|'script'} secondStage[][0]] - The resource type.
+       * @property {string} secondStage.[][1]] - The resource path.
+       *
+       * @property {function} add - Add resource to the stage.
+       */
       self._resources = {
+        /** @member {IncludeMocha#_stageResourceList} IncludeMocha#_Resources#firstStage
+         */
         firstStage: [],
         secondStage: [],
+        /** @method IncludeMocha#_resources.add
+         *
+         */
         add: function( stage, type, source ){
           this[stage] = [ type, source ];
         }
@@ -155,12 +213,33 @@
       });
 
       self._loadResources( 'firstStage' );
-    };
+      
+      return;
+    }; //-- self
     self.__proto__ = IncludeMocha.prototype;
     return self;
   };
 
   IncludeMocha.prototype = Object.create(Function.prototype);
+  
+  IncludeMocha.prototype.onComplete = function( callback ){
+    this._completeCallback = callback;
+  };
+  
+  IncludeMocha.prototype._complete = function( error ){
+    if ( !error ){
+      this._completeCallback( {hasError: false} );
+      return;
+    } else if ( !self._hasError ){
+      this._hasError = true;
+      this._completeCallback({
+        hasError: true,
+        error: error
+      });
+    } else {
+      return;
+    }
+  }
 
   IncludeMocha.prototype._loadResources = function( stage ){
 
@@ -205,7 +284,12 @@
 
     for (var i=0; i<nodes.length; i++){
       nodes[i].addEventListener('error', function(){
-        self._foundResourceLoadError( nodes[i] );
+        if ( node[i].tagName === 'script' ){
+          var source = node[i].getAttribute('src')
+        } else {
+          var source = node[i].getAttribute('href')
+        }
+        self._complete( new Error('Не удалось загрузить ресурс из источника "' +source +'".') );
       });
     }
   };
